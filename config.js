5f055e7e462ae8bcb61a77de162fd2e02d8c343c
vishal-m25 <vellimuthu9@gmail.com>
2025-05-21 20:20:23 +0530

    mongodb connection

--- config.js
+++ config.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI_LOCAL;
+  const uri = process.env.MONGO_URI_REMOTE;
 
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log(`MongoDB connected`);
   } catch (err) {
     console.error('MongoDB connection error:', err);
--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.use((req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
 });
